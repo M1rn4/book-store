d1d63a2c1a48dad83715498284e0631bf6fe293c
mirnamp <ampueromirna8@gmail.com>
2023-02-03 16:14:16 -0500

    changes required

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => <button type="button">Check Status</button>;
+const Categories = () => {
+  const categories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button type="button" onClick={() => dispatch(checkStatus())}>Check Status</button>
+      <h1>{categories}</h1>
+    </div>
+  );
+};
 
 export default Categories;
